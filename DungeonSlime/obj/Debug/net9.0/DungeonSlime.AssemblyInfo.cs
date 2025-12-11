ff8d2dc55943be0c70c94270aa7275e415bae796
gooseish <cegooseish@gmail.com>
2025-12-10 23:42:03 -0500

    added core, though the tutorial wanted it to be in a separate library, that didn't work for some reason

--- DungeonSlime/obj/Debug/net9.0/DungeonSlime.AssemblyInfo.cs
+++ DungeonSlime/obj/Debug/net9.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55bc3b92d499dfa7f65c24d0f9fff7ef2b4fb5e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1810e1a2536dfafef42713aeda297dde63062dce")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
