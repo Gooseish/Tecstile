60034e69d5e583b123f1daea0c0fc07a9ff16d20
gooseish <cegooseish@gmail.com>
2025-12-14 11:06:09 -0500

    start setting up managers and states for content

---
+++ Tecstile/Source Code/Graphics/GraphicalContentManager.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Tecstile.Graphics;
+
+public class GraphicalContentManager
+{
+    private GraphicalContentState State;
+
+    #region Accessors
+    #endregion
+
+    #region Public Controls
+    #endregion
+
+    #region Private Controls
+    #endregion
+
+    public GraphicalContentManager()
+    {
+        State = new GraphicalContentState();
+    }
+}
---
+++ Tecstile/Source Code/Graphics/GraphicalContentState.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tecstile.Graphics;
+
+public class GraphicalContentState
+{
+    public Dictionary<string, Texture2D> menuTextures;
+}
