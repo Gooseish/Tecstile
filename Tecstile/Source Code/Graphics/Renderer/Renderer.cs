d154d8877dca34ccc5d65b52b25cbd69b5209520
gooseish <cegooseish@gmail.com>
2025-12-14 20:49:15 -0500

    Support for game window resizing with letterboxing

--- Tecstile/Source Code/Graphics/Renderer/Renderer.cs
+++ Tecstile/Source Code/Graphics/Renderer/Renderer.cs
@@ -2,16 +2,38 @@ using System;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Tecstile.Scene;
+using System.Collections.Generic;
+using Tecstile.Config;
 
 namespace Tecstile.Graphics;
 
 public static partial class Renderer
 {
+    #region Render Targeting
+    private static RenderTarget2D[] StableRenderTargets = new RenderTarget2D[2];
+    private static RenderTarget2D[] VolatileRenderTargets = new RenderTarget2D[2];
+    private static int StableIndex = 0;
+    private static int VolatileIndex = 0;
+    #endregion
+    #region Drawing
     public static void draw(GraphicsDevice graphicsDevice)
     {
+        RenderTarget2D renderTarget = new RenderTarget2D(
+            graphicsDevice, TecstileConfig.sourceResolutionWidth, TecstileConfig.sourceResolutionHeight);
+
+        graphicsDevice.SetRenderTarget(renderTarget);
         graphicsDevice.Clear(Color.CornflowerBlue);
+        
         SpriteBatch spriteBatch = new SpriteBatch(graphicsDevice);
 
+        DrawAtSourceResolution(spriteBatch);
+
+        graphicsDevice.SetRenderTarget(null);
+
+        DrawToTargetResolution(spriteBatch, renderTarget);
+    }
+    private static void DrawAtSourceResolution(SpriteBatch spriteBatch)
+    {
         BySceneType();
         void BySceneType()
         {
@@ -26,4 +48,11 @@ public static partial class Renderer
             DrawMenus(spriteBatch);
         }
     }
+    private static void DrawToTargetResolution(SpriteBatch spriteBatch, RenderTarget2D renderTarget)
+    {
+        spriteBatch.Begin();
+        spriteBatch.Draw(renderTarget, Global.settings.letterboxPicture, Color.White);
+        spriteBatch.End();
+    }
+    #endregion
 }
