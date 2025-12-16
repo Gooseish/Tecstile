fba0ab1134373167f6d8e7be7a5cb46f3d8c1815
gooseish <cegooseish@gmail.com>
2025-12-16 13:04:06 -0500

    setup avalonia project and gitignore

---
+++ TecstileEditor/TecstileEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TecstileEditor.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ TecstileEditor/TecstileEditor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TecstileEditor.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
