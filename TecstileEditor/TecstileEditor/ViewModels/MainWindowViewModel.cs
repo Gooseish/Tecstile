7ced9723ac5ce8b525d340e0ddc64bb0e2d1cc44
gooseish <cegooseish@gmail.com>
2025-12-17 10:13:05 -0500

    menu bar and tabs

--- TecstileEditor/TecstileEditor/ViewModels/MainWindowViewModel.cs
+++ TecstileEditor/TecstileEditor/ViewModels/MainWindowViewModel.cs
@@ -5,24 +5,5 @@ namespace TecstileEditor.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string? _celsius;
-    [ObservableProperty]
-    private string? _fahrenheit;
-
-    [RelayCommand]
-    private void Calculate()
-    {
-        if (double.TryParse(Celsius, out double C))
-        {
-            var F = C * (9d / 5d) + 32;
-            Fahrenheit = F.ToString("0.0");
-        }
-        else
-        {
-            Fahrenheit = "0";
-            Celsius = "0";
-        }
-    }
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    
 }
