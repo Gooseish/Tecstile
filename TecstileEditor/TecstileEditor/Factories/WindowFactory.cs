37ab2d783ab583b3288462d65ac2c55d8d5610c5
gooseish <cegooseish@gmail.com>
2025-12-20 16:02:30 -0500

    continue setting up window factory

--- TecstileEditor/TecstileEditor/Factories/WindowFactory.cs
+++ TecstileEditor/TecstileEditor/Factories/WindowFactory.cs
@@ -1,9 +1,21 @@
 using System;
 using TecstileEditor.Data;
+using TecstileEditor.Views;
 
 namespace TecstileEditor.Factories;
 
 public class WindowFactory
 {
-    
+    private readonly Func<EditorName, ChildWindow> windowFactory;
+
+    public WindowFactory(Func<EditorName, ChildWindow> factory)
+    {
+        windowFactory = factory;
+    }
+
+    public void LaunchNewChildWindow(EditorName name)
+    {
+        var window = windowFactory.Invoke(name);
+        window.Show();
+    }
 }
