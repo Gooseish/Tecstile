40b9238aec0539486a2772c316f4832e448abe31
gooseish <cegooseish@gmail.com>
2025-12-21 17:27:10 -0500

    attempt to serialize terrain data as json

--- TecstileEditor/TecstileEditor/Services/SaveLoadService.cs
+++ TecstileEditor/TecstileEditor/Services/SaveLoadService.cs
@@ -1,16 +1,23 @@
 using System;
 using System.IO;
+using System.Text.Json;
+using TecstileEditor.Models;
 
 namespace TecstileEditor.Services;
 
 public static class SaveLoadService
 {
-    public static string ContentRoot = @"Content/";
+    public static string? ContentRoot = @"Content/";
     public static void Save()
     {
-        System.Console.WriteLine("Ding!");
-
-        string writeText = "Hello World!";
-        File.WriteAllText(ContentRoot + "filename.txt", writeText);
+        if (ContentRoot == null)
+            return; // Prompt to pick new folder here
+    
+        string jsonString = JsonSerializer.Serialize(TerrainDataModel.terrainData);
+        File.WriteAllText(ContentRoot + "filename.txt", jsonString);
+    }
+    public static void Open()
+    {
+        
     }
 }
