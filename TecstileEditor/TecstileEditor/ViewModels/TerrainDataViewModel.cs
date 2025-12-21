7de17dbc334e26627616e4b52d5cf479f5dcf00f
gooseish <cegooseish@gmail.com>
2025-12-21 16:46:23 -0500

    tie terrain data view model to terrain data model loaded in memory

--- TecstileEditor/TecstileEditor/ViewModels/TerrainDataViewModel.cs
+++ TecstileEditor/TecstileEditor/ViewModels/TerrainDataViewModel.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Tecstile.Game_Board;
 using TecstileEditor.Models;
 
 namespace TecstileEditor.ViewModels;
 
-public partial class TerrainDataViewModel : ViewModelBase
+public partial class TerrainDataViewModel : ObservableObject
 {
     /// <summary>
     /// Create a new TerrainDataViewModel by creating a new
@@ -39,20 +41,53 @@ public partial class TerrainDataViewModel : ViewModelBase
     #region Boilerplate Properties
     [ObservableProperty]
     private int _id;
+    partial void OnIdChanged(int oldValue, int newValue)
+    {
+        TerrainDataModel.terrainData.Remove(oldValue);
+        TerrainDataModel.terrainData[newValue] = GetTerrain();
+    }
     [ObservableProperty]
     private string _name;
+    partial void OnNameChanged(string? oldValue, string newValue)
+    {
+        TerrainDataModel.terrainData[Id].name = newValue;
+    }
     [ObservableProperty]
     private int _avoid;
+    partial void OnAvoidChanged(int oldValue, int newValue)
+    {
+        TerrainDataModel.terrainData[Id].avoid = newValue;
+    }
     [ObservableProperty]
     private int _def;
+    partial void OnDefChanged(int oldValue, int newValue)
+    {
+        TerrainDataModel.terrainData[Id].def = newValue;
+    }
     [ObservableProperty]
     private int _res;
+    partial void OnResChanged(int oldValue, int newValue)
+    {
+        TerrainDataModel.terrainData[Id].res = newValue;
+    }
     [ObservableProperty]
     private bool _heals;
+    partial void OnHealsChanged(bool oldValue, bool newValue)
+    {
+        TerrainDataModel.terrainData[Id].heals = newValue;
+    }
     [ObservableProperty]
     private int _healPercent;
+    partial void OnHealPercentChanged(int oldValue, int newValue)
+    {
+        TerrainDataModel.terrainData[Id].healPercent = newValue;
+    }
     [ObservableProperty]
     private Dictionary<MovementType, int> _movementCost;
+    partial void OnMovementCostChanged(Dictionary<MovementType, int>? oldValue, Dictionary<MovementType, int> newValue)
+    {
+        TerrainDataModel.terrainData[Id].movementCost = newValue;
+    }
     #endregion
     public Terrain GetTerrain()
     {
